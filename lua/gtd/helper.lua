d6c74c41aa1f5f497d00bfc41b354d7f8e0a711c
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2023-01-27 20:28:18 +0900

    Add support 'git' filetype

--- lua/gtd/helper.lua
+++ lua/gtd/helper.lua
@@ -3,7 +3,7 @@ local helper = {}
 ---@param context gtd.Context
 function helper.fix_diff(context)
   if context.fname then
-    if vim.tbl_contains({ 'diff', 'gitcommit' }, vim.bo[context.bufnr].filetype) then
+    if vim.tbl_contains({ 'diff', 'git', 'gitcommit' }, vim.bo[context.bufnr].filetype) then
       if context.fname:match('^a/') then
         context.fname = context.fname:gsub('^a/', '')
       elseif context.fname:match('^b/') then
